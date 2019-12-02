9bc271105f4d897f730377563345280cbeeb4a3b
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2019-12-01 20:50:05 -0800

    fixed the page, mobile styling pass

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,22 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound: React.FunctionComponent = () => {
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignContent: "center",
-        justifyContent: "center",
-        textAlign: "center"
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        height: '100vh',
+        color: 'white'
       }}
     >
-      <h1 style={{ padding: "1em" }}>Resource Not Found</h1>
-      <p style={{ paddingBottom: "0.5em" }}>
+      <h1 style={{ padding: '1em' }}>Resource Not Found</h1>
+      <p style={{ paddingBottom: '0.5em' }}>
         The resource you specified cannot be found.
       </p>
-      <Link to="/">Click here to go back home.</Link>
+      <Link to='/'>Click here to go back home.</Link>
     </div>
   );
 };
--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Spinner from "../Shared/Spinner/Spinner";
-import Pages from "../../content/Pages.json";
-import GetPages from "./utils/GetPages";
-import { Link } from "react-router-dom";
-import "./styles/page.css";
+import React from 'react';
+import Spinner from '../Shared/Spinner/Spinner';
+import Pages from '../../content/Pages.json';
+import GetPages from './utils/GetPages';
+import { Link } from 'react-router-dom';
+import './styles/page.css';
 
 interface PageContent {
   title: string;
@@ -28,15 +28,15 @@ const Page: React.FunctionComponent<Props> = props => {
 
   let currentPageContent: PageContent = GetPages(match.params.id);
   return (
-    <React.Fragment>
-      <div className="paper">
+    <div className='page'>
+      <div className='paper'>
         <h2>{currentPageContent.title}</h2>
         <p>{currentPageContent.body}</p>
-        <Link style={{ marginBottom: "1.5em" }} to="/">
+        <Link style={{ marginBottom: '1.5em' }} to='/'>
           Go Back Home
         </Link>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
